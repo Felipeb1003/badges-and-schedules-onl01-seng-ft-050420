9b13f54be6b100ee8703e0b7049a544dffaeee86
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-08 19:03:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker (name)
+    "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(array)
+    array.collect do |name|
+      badge_maker (name)
+    end
+    
+end
+
+def assign_rooms (array)
+    room_number = 0
+    array.collect do |arrays|
+     "Hello, #{arrays}! You'll be assigned to room #{room_number +=1}!"   
+    end  
+    
+end    
+
+def printer (attendee)
+    badges_and_rooms = []
+    badges_and_rooms << badge_maker(attendee)
+    badges_and_rooms << assign_rooms(attendee)
+    badges_and_rooms.each do |answer|
+      puts "#{answer}"
+    end
+  
+end
