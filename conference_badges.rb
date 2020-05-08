737191a3ec0cdb4c601648c53db756cebe7a5da7
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-08 19:08:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ end
 
 def printer (attendee)
     badges_and_rooms = []
-    badges_and_rooms << badge_maker(attendee)
+    badges_and_rooms << batch_badge_creator(attendee)
     badges_and_rooms << assign_rooms(attendee)
     badges_and_rooms.each do |answer|
       puts "#{answer}"
